f4d970cddff2e1792df382a070e50008385502d1
ANDREI\neamt <neamtuxandrei@gmail.com>
2023-08-06 23:41:47 +0300

    latestChanges

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  username = '';
+  allowButton = true;
+  constructor() {}
 
+  onChangeUsername() {
+    this.username = 'UserName was changed!';
+  }
+  onButtonActive() {
+    if (this.allowButton) this.allowButton = false;
+    else this.allowButton = true;
+  }
+  onClearUsername() {
+    this.username = '';
+  }
 }
